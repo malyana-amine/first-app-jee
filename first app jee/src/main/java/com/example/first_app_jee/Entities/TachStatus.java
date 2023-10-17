3446d948e5302e3ce398c87c96d8e061e2bee9ff
amine malyana <aminemalyana12@gmail.com>
2023-10-17 20:50:50 +0100

    data base error

---
+++ first app jee/src/main/java/com/example/first_app_jee/Entities/TachStatus.java
@@ -0,0 +1,5 @@
+package com.example.first_app_jee.Entities;
+
+public enum TachStatus {
+    TODO,DOING,DONE
+}
--- first app jee/src/main/java/com/example/first_app_jee/Entities/Task.java
+++ first app jee/src/main/java/com/example/first_app_jee/Entities/Task.java
@@ -2,40 +2,50 @@ package com.example.first_app_jee.Entities;
 
 import jakarta.persistence.*;
 import java.util.Date;
-import java.util.List;
 
 @Entity
-@Table(name = "Reservations")
-public class Reservation {
+@Table(name = "Tasks")
+public class Task {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "description")
+    private String description;
+    @Column(name = "status")
+    private TachStatus status;
+
     @Column(name = "start_date")
     private Date startDate;
 
-    @Column(name = "end_date")
-    private Date endDate;
+    @Column(name = "due_date")
+    private Date dueDate;
 
     @ManyToOne
     @JoinColumn(name = "user_id") // This should match the name of the foreign key column in your database
     private Users user;
 
-    @OneToMany(mappedBy = "reservation")
-    private List<Equipment> equipmentList;
-
-    public Reservation(Date startDate, Date endDate, Users user) {
+    public Task(String description, Date startDate, Date dueDate, Users user) {
+        this.description = description;
         this.startDate = startDate;
-        this.endDate = endDate;
+        this.dueDate = dueDate;
         this.user = user;
     }
 
-    public Reservation() {
+    public Task() {
 
     }
 
     // Getter and setter methods for the fields
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
     public Date getStartDate() {
         return startDate;
@@ -45,12 +55,12 @@ public class Reservation {
         this.startDate = startDate;
     }
 
-    public Date getEndDate() {
-        return endDate;
+    public Date getDueDate() {
+        return dueDate;
     }
 
-    public void setEndDate(Date endDate) {
-        this.endDate = endDate;
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
     }
 
     public Users getUser() {
@@ -59,14 +69,5 @@ public class Reservation {
 
     public void setUser(Users user) {
         this.user = user;
-    }
-
-
-    public List<Equipment> getEquipmentList() {
-        return equipmentList;
-    }
-
-    public void setEquipmentList(List<Equipment> equipmentList) {
-        this.equipmentList = equipmentList;
     }
 }
