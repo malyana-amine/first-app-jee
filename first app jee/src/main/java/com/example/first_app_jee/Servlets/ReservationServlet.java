512c2dfbd1107e0975fdb7b3a0cefde85c09269e
amine malyana <aminemalyana12@gmail.com>
2023-10-20 16:43:37 +0100

    reservation front

---
+++ first app jee/src/main/java/com/example/first_app_jee/Servlets/ReservationServlet.java
@@ -0,0 +1,20 @@
+package com.example.first_app_jee.Servlets;
+
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "reservation", urlPatterns = {"/reservation"} )
+public class ReservationServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/Reservation.jsp");
+        dispatcher.forward(request,response);
+    }
+}
